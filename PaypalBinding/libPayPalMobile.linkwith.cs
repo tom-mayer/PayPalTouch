6c82ac51d43b396df9d4b6a7a6f10b7b34e8b4d0
tom_mayer <tom.mayer@digitalpatrioten.com>
2014-06-03 08:58:19 +0200

    added another framework required by paypal according to docs

--- PaypalBinding/libPayPalMobile.linkwith.cs
+++ PaypalBinding/libPayPalMobile.linkwith.cs
@@ -2,6 +2,6 @@ using System;
 using MonoTouch.ObjCRuntime;
 
 [assembly: LinkWith("libPayPalMobile.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, 
-    Frameworks = "AVFoundation CoreLocation SystemConfiguration",
-    LinkerFlags = "-lc++"
+    Frameworks = "AVFoundation CoreLocation SystemConfiguration MessageUI",
+    LinkerFlags = "-lc++ -ObjC"
 )]
